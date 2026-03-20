7f8f236f92663cd42eda0fc707e9a6805fbdaa84
그필 <gracefield0814@gmail.com>
2026-03-20 18:06:02 +0900

    test 파일 주석 처리

--- seoul_bike_prediction/test/widget_test.dart
+++ seoul_bike_prediction/test/widget_test.dart
@@ -1,30 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:seoul_bike_prediction/main.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// void main() {
+//   testWidgets('initial route shows auth page', (WidgetTester tester) async {
+//     await tester.pumpWidget(const SeoulBikePredictionApp());
+//     await tester.pumpAndSettle();
 
-import 'package:seoul_bike_prediction/main.dart';
+//     expect(find.text('Auth Page'), findsOneWidget);
+//     expect(find.text('Current route: /auth'), findsOneWidget);
+//   });
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+//   testWidgets('tap navigation button moves to dashboard page', (
+//     WidgetTester tester,
+//   ) async {
+//     await tester.pumpWidget(const SeoulBikePredictionApp());
+//     await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     await tester.tap(find.text('Dashboard'));
+//     await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//     expect(find.text('Dashboard Page'), findsOneWidget);
+//     expect(find.text('Current route: /dashboard'), findsOneWidget);
+//   });
+// }
