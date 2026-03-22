717adc1fc3a5e699879a43aa00b8ca3e20ea7864
KorWeiss <signalizer@hanmail.net>
2026-03-22 17:07:49 +0900

    22-Mar-2026 Chansol Park desc:Init update for Dashboard.dart *** Changed .gitignore to track any .pods files, .xcconfig files ***

--- seoul_bike_prediction/lib/features/dashboard/dashboard_page.dart
+++ seoul_bike_prediction/lib/features/dashboard/dashboard_page.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:seoul_bike_prediction/routes/demo_nav_scaffold.dart';
-import 'package:seoul_bike_prediction/routes/router_names.dart';
+import 'package:seoul_bike_prediction/view/restitutor/dashboard.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DemoNavScaffold(
-      title: 'Dashboard Page',
-      currentRoute: RouterNames.dashboard,
-    );
+    return const Dashboard();
   }
 }
